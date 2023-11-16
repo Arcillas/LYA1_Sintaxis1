8f98d504a0366e70986b085a3231f64aed5fcfe2
NinjagoRT <lloydmongomerigarmadon7@gmail.com>
2023-11-16 17:35:33 -0600

    Cambiando las producciones de la unidad 6

--- prueba.cpp
+++ prueba.cpp
@@ -1,18 +1,53 @@
 #include <stdio.h>
 #include <math.h>
-#include <stdio>
-#include <math>
 
-char  a;
-int   a,b,c,d,radio;
-float x,y,z;
-char  xd;
+int altura,i,j;
+float x,y;
 
 void main()
 {
-    printf("\nHola mundo");
-    printf("\n============");
-    printf("\nRadio = ");
-    printf("Radio %i, y altura: %i", x ,zs);
-    scanf("%i",&radio);
+    y = 10;
+    x = 9;
+    x += (3 + 5) * 8 - (10 - 4) / 2; 
+    x /= (y-3); 
+    x --; 
+    x *= (x-7);
+    
+    printf("\nValor de altura = ");
+    scanf("%d",&altura);
+
+    for (i = 1; i<=altura; i++)
+    {
+        for (j = 1; j<=i; j++)
+        {
+            printf("",j);
+        }
+        printf("\n");
+    }
+    i = 0;
+    do
+    {
+        printf("-");
+        i++;
+    }
+    while (i<altura*2);
+    printf("\n");
+    for (i = 1; i<=altura; i++)
+    {
+        j = 1;
+        while (j<=i)
+        { 
+            printf("",j);
+            j++;
+        }
+        printf("\n");
+    }
+    i = 0;
+    do
+    {
+        printf("-");
+        i++;
+    }
+    while (i<altura*2);
+    printf("\n");
 }
